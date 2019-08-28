32e9aca687d1ae69abf7260db4afb2e19f77408c
kendypham <kendypham050197@gmail.com>
2019-08-28 22:03:43 +0700

    fix bug store data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ var arrayList = [];
 var completed = [];
 var test = []
 var inputElement = document.getElementById("inputTodo");
-renderList(arrayList);
-function storeData() {
-    localStorage.setItem("items", JSON.stringify(arrayList))
-    localStorage.setItem("complete", JSON.stringify(completed))
-}
+renderList("items");
+
 //add item to list
 inputElement.addEventListener("keyup", function (e) {
     if (e.key === "Enter") {
@@ -17,7 +14,7 @@ inputElement.addEventListener("keyup", function (e) {
         arrayList.push(obj);
         storeData()
         clean()
-        renderList(arrayList);
+        renderList("items");
     }
 })
 //remove item from list
@@ -27,9 +24,10 @@ removeItem = (id) => {
             return item
     });
     completed.push(item);
-    arrayList.splice(id, 1);
+    let pos = arrayList.indexOf(item)
+    arrayList.splice(pos, 1);
     storeData()
-    renderList(arrayList)
+    renderList("items")
 }
 
 //edit item
@@ -38,6 +36,7 @@ editItem = (id) => {
         if (item.id === id)
             return item
     })
+   
     document.getElementById(item.id).innerHTML = `<input id="input${item.id}" value="${item.value}"/>
     <button class="float-right btn btn-success ml-2" 
     onclick="saveEdit(${item.id})">Save
@@ -67,7 +66,8 @@ saveEdit = (id) => {
             return item
     })
     item.value = document.getElementById(`input${item.id}`).value;
-    arrayList.splice(id,1,item);
+    let pos = arrayList.indexOf(item)
+    arrayList.splice(pos, 1, item);
     storeData();
     document.getElementById(item.id).innerHTML = `${item.value} 
     <button class="float-right fas fa-trash btn btn-danger ml-2" 
@@ -79,22 +79,45 @@ saveEdit = (id) => {
 }
 
 //render list
-function renderList(list) {
-    let arrObj = JSON.parse(localStorage.getItem("items"));
-    if(arrObj != null){
-        list = [...arrObj];
+function renderList(name) {
+    let list = [];
+    if (name === "items") {
+        let arrObj = JSON.parse(localStorage.getItem("items"));
+        if (arrObj != null) {
+            list = [...arrObj];
+            list.sort(function (a, b) {
+                return a.id - b.id;
+            });
+            arrayList = list;
+        }
+        test = list.map((item, index) => {
+            return `<li class="list-group-item" 
+            id="${item.id}">${item.value} 
+            <button class="float-right fas fa-trash btn btn-danger ml-2" 
+            onclick="removeItem(${item.id})">
+            </button>
+            <button class="float-right fas fa-pencil-alt btn btn-warning" 
+            onclick="editItem(${item.id})">
+            </button></li>`
+        })
     }
-  
-    test = list.map((item, index) => {
-        return `<li class="list-group-item" 
-        id="${item.id}">${item.value} 
-        <button class="float-right fas fa-trash btn btn-danger ml-2" 
-        onclick="removeItem(${item.id})">
-        </button>
-        <button class="float-right fas fa-pencil-alt btn btn-warning" 
-        onclick="editItem(${item.id})">
-        </button></li>`
-    })
+    else {
+        let arrObj = JSON.parse(localStorage.getItem("complete"));
+        if (arrObj != null) {
+            list = [...arrObj];
+            list.sort(function (a, b) {
+                return a.id - b.id;
+            });
+            completed = list;
+        }
+        test = list.map((item, index) => {
+            return `<li class="list-group-item" 
+            id="${item.id}">${item.value} 
+            <button class="float-right fas fa-undo btn btn-info" 
+            onclick="undo(${item.id})">
+            </button></li>`
+        })
+    }
     let str = "";
     test.forEach(item => {
         str += item
@@ -106,9 +129,34 @@ function clean() {
     inputElement.value = ""
 }
 
+undo = (id) => {
+    let item = completed.find(item => {
+        if (item.id === id)
+            return item
+    })
+    arrayList.push(item);
+    let pos = completed.indexOf(item)
+    completed.splice(pos, 1);
+    storeData()
+    renderList("complete")
+
+}
 
 showCompleted = () => {
-    console.log(completed);
-    
-    renderList(completed)
+    renderList("complete")
+}
+
+showAll = () => {
+    renderList("items")
+}
+
+function storeData() {
+    arrayList.sort(function (a, b) {
+        return a.id - b.id;
+    });
+    completed.sort(function (a, b) {
+        return a.id - b.id;
+    });
+    localStorage.setItem("items", JSON.stringify(arrayList))
+    localStorage.setItem("complete", JSON.stringify(completed))
 }
